4ec1296c584382c0d01f9a9e0e039ed1b44eb99f
duaa-tarazi <dst08itm@yahoo.com>
2021-06-07 23:44:21 +0300

    change the images

--- app.js
+++ app.js
@@ -22,22 +22,26 @@ function Goods(name, imagelink) {
 
 Goods.allGoods = [];
 
-new Goods('bag', '/images/bag.jpg');
-new Goods('banana', '/images/banana.jpg');
-new Goods('bathroom', '/images/bathroom.jpg');
-new Goods('boots', '/images/boots.jpg');
-new Goods('breakfast', '/images/breakfast.jpg');
-new Goods('bubblegum', '/images/bubblegum.jpg');
-new Goods('chair', '/images/chair.jpg');
-new Goods('cthulhu', '/images/cthulhu.jpg');
-new Goods('dog-duck', '/images/dog-duck.jpg');
-new Goods('dragon', '/images/dragon.jpg');
-new Goods('pen', '/images/pen.jpg');
-new Goods('pet-sweep', '/images/pet-sweep.jpg');
-new Goods('scissors', '/images/scissors.jpg');
-new Goods('usb', '/images/usb.gif');
-new Goods('water-can', '/images/water-can.jpg');
-new Goods('wine-glass', '/images/wine-glass.jpg');
+new Goods('bag', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bag.jpg');
+new Goods('banana', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/banana.jpg');
+new Goods('bathroom', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bathroom.jpg');
+new Goods('boots', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/boots.jpg');
+new Goods('breakfast', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/breakfast.jpg');
+new Goods('bubblegum', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bubblegum.jpg');
+new Goods('chair', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/chair.jpg');
+new Goods('cthulhu', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/dragon.jpg');
+new Goods('dog-duck', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/dog-duck.jpg');
+new Goods('dragon', 'https://github.com/LTUC/amman-201d27/blob/main/class-11/assets/cthulhu.jpg?raw=true');
+new Goods('pen', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/pen.jpg');
+new Goods('pet-sweep', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/pet-sweep.jpg');
+new Goods('scissors', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/scissors.jpg');
+new Goods('usb', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/usb.gif');
+new Goods('water-can', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/water-can.jpg');
+new Goods('wine-glass', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/wine-glass.jpg');
+new Goods('shark', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/shark.jpg');
+new Goods('sweep', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/sweep.png');
+new Goods('tauntaun', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/tauntaun.jpg');
+new Goods('unicorn', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/unicorn.jpg');
 console.log(Goods.allGoods);
 
 

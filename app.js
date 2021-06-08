3d12dcee6e65bdf038b6a3b9cf731b8c85cdd08e
duaa-tarazi <dst08itm@yahoo.com>
2021-06-08 18:01:22 +0300

    fix sho result fun

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 let leftImageElement = document.getElementById("myImage1");
 let centerImageElement = document.getElementById("myImage2");
 let rightImageElement = document.getElementById("myImage3");
-let container=document.getElementById("imgcontainer");
+let container = document.getElementById("imgcontainer");
 
 
 let leftIndex;
@@ -12,8 +12,8 @@ let rightIndex;
 
 let rounds = 25;
 let countclick = 0;
-let arrOfNames=[];
-let arrOfVotes=[];
+let arrOfNames = [];
+let arrOfVotes = [];
 
 function Goods(name, imagelink) {
   this.name = name;
@@ -26,26 +26,23 @@ function Goods(name, imagelink) {
 
 Goods.allGoods = [];
 
-new Goods('bag', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bag.jpg');
-new Goods('banana', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/banana.jpg');
-new Goods('bathroom', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bathroom.jpg');
-new Goods('boots', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/boots.jpg');
-new Goods('breakfast', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/breakfast.jpg');
-new Goods('bubblegum', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/bubblegum.jpg');
-new Goods('chair', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/chair.jpg');
-new Goods('cthulhu', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/dragon.jpg');
-new Goods('dog-duck', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/dog-duck.jpg');
-new Goods('dragon', 'https://github.com/LTUC/amman-201d27/blob/main/class-11/assets/cthulhu.jpg?raw=true');
-new Goods('pen', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/pen.jpg');
-new Goods('pet-sweep', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/pet-sweep.jpg');
-new Goods('scissors', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/scissors.jpg');
-new Goods('usb', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/usb.gif');
-new Goods('water-can', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/water-can.jpg');
-new Goods('wine-glass', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/wine-glass.jpg');
-new Goods('shark', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/shark.jpg');
-new Goods('sweep', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/sweep.png');
-new Goods('tauntaun', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/tauntaun.jpg');
-new Goods('unicorn', 'https://raw.githubusercontent.com/LTUC/amman-201d27/main/class-11/assets/unicorn.jpg');
+new Goods('bag', 'images/bag.jpg');
+new Goods('banana', 'images/banana.jpg');
+new Goods('bathroom', 'images/bathroom.jpg');
+new Goods('boots', 'images/boots.jpg');
+new Goods('breakfast', 'images/breakfast.jpg');
+new Goods('bubblegum', 'images/bubblegum.jpg');
+new Goods('chair', 'images/chair.jpg');
+new Goods('dragon', 'images/dragon.jpg');
+new Goods('dog-duck', 'images/dog-duck.jpg');
+new Goods('cthulhu', 'images/cthulhu.jpg');
+new Goods('pen', 'images/pen.jpg');
+new Goods('pet-sweep', 'images/pet-sweep.jpg');
+new Goods('usb', 'images/usb.gif');
+new Goods('water-can', 'images/water-can.jpg');
+new Goods('wine-glass', 'images/wine-glass.jpg');
+
+console.log(Goods.allGoods);
 
 
 function generateRandomIndex() {
@@ -62,18 +59,17 @@ function showthreeImages() {
   rightIndex = generateRandomIndex();
 
   while (leftIndex === centerIndex || leftIndex === rightIndex || rightIndex === centerIndex) {
-    leftIndex = generateRandomIndex();
-    centerIndex = generateRandomIndex();
+     leftIndex = generateRandomIndex();
+     centerIndex = generateRandomIndex();
 
-  }}
-
+   }
   leftImageElement.setAttribute('src', Goods.allGoods[leftIndex].imagelink);
   Goods.allGoods[leftIndex].shown++;
   centerImageElement.setAttribute('src', Goods.allGoods[centerIndex].imagelink);
   Goods.allGoods[centerIndex].shown++;
   rightImageElement.setAttribute('src', Goods.allGoods[rightIndex].imagelink);
   Goods.allGoods[rightIndex].shown++;
-
+}
 showthreeImages();
 container.addEventListener('click', handleClicking);
 let button;
@@ -81,50 +77,56 @@ let button;
 
 function handleClicking(event) {
   countclick++;
-if(rounds>=countclick){
-  if (event.target.id==='myImage1'){
-  Goods.allGoods[leftIndex].votes++;}
-else if (event.target.id==='myImage2'){
-  Goods.allGoods[centerIndex].votes++;}
-else if (event.target.id==='myImage3'){
-  Goods.allGoods[rightIndex].votes++;}
-else{
-  countclick--;
-  return
-}
-showthreeImages();
-}
-showresult();
+  if (rounds >= countclick) {
+    if (event.target.id === 'myImage1') {
+      Goods.allGoods[leftIndex].votes++;
+    }
+    else if (event.target.id === 'myImage2') {
+      Goods.allGoods[centerIndex].votes++;
+    }
+    else if (event.target.id === 'myImage3') {
+      Goods.allGoods[rightIndex].votes++;
+    }
+    else {
+      countclick--;
+      return
+    }
+    showthreeImages();
+  }
 
-}
-//else{
-  //button=document.getElementById('butoon');
-  //button.addEventListener('click,handleShowing')
- // container.removeEventListener('click',handleClicking);
-//}
+else{
+let button=document.getElementById('butoon');
+//button.addEventListener("click",handleShowing);
+//button.onclick=handleShowing;
+showresult();
+ //container.removeEventListener('click',handleClicking);
+}}
 
 
 
 function handleShowing() {
-  gettingList();
-  gettingchart();
-  button.removeEventListener('click,handleshowing');
+  showresult();
+  console.log(hi);
+  chart();
+  console.log(hi);
+  //button.removeEventListener("click",handleshowing);
 
 }
-let arrOfSeen=[];
-function showresult()
-{
-  let ul=document.getElementById('result');
-  for (let i=0;i<Goods.allGoods.length;i++){
+let arrOfSeen = [];
+
+function showresult() {
+  let ul = document.getElementById('result');
+  for (let i = 0; i < Goods.allGoods.length; i++) {
     arrOfVotes.push(Goods.allGoods[i].votes);
     arrOfSeen.push(Goods.allGoods[i].shown);
-    let li=document.createElement('li');
+    let li = document.createElement('li');
     ul.appendChild(li);
-    li.textContent=`${Goods.allGoods[i].name} has ${Goods.allGoods[i].votes} Votes and it has been shown ${Goods.allGoods[i].shown}`;
+    li.textContent = `${Goods.allGoods[i].name} has ${Goods.allGoods[i].votes} Votes and it has been shown ${Goods.allGoods[i].shown}`;
   }
 }
-function chart{
-  
+function chart()
+{
+
 let ctx = document.getElementById('myChart')
 let myChart = new Chart(ctx, {
     type: 'bar',
@@ -150,5 +152,5 @@ let myChart = new Chart(ctx, {
 });
 }
 
-}
+
 
